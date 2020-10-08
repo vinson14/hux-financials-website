e56a45e9e59239bfb37de839b116648cfc66d02c
Vinson <vinson1493@gmail.com>
2020-10-08 12:53:16 +0800

    Added background image to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,14 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import Header from "./micro-components/Header";
 import ContactForm from "./micro-components/ContactForm";
+import "../static/css/contact.css";
 
 class Contact extends React.Component {
     render() {
         return (
-            <div>
+            <div className="h-100 contact-background">
                 <Header position="sticky" bg="white" />
-                <Container>
+                <Container >
                     <Row>
                         <Col className="d-flex justify-content-center">
                             <ContactForm />
