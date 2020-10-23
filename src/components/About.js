b09594ed0aedf205e29f60a37d2c5b50002a422e
Vinson <vinson1493@gmail.com>
2020-10-23 17:46:14 +0800

    Added tabletop

--- src/components/About.js
+++ src/components/About.js
@@ -1,67 +1,56 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { GoogleSpreadsheet } from "google-spreadsheet";
-
+import Tabletop from "tabletop";
 import ProfileCard from "./micro-components/ProfileCard";
 import Header from "./micro-components/Header";
 import Footer from "./micro-components/Footer";
 
-
 class About extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            profiles: [],
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      profiles: [],
 
-    getPeople = async () => {
-        const doc = new GoogleSpreadsheet(
-            process.env.REACT_APP_GOOGLE_SPREADSHEET_ID
-        );
-        await doc.useServiceAccountAuth({
-            private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
-            client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        });
-
-        await doc.loadInfo();
-
-        const sheet = doc.sheetsByIndex[0];
-        const rows = await sheet.getRows();
-
-        this.setState({
-            profiles: rows
-        })
-        console.log(this.state.profiles)
     };
+  }
 
-    componentDidMount() {
-        this.getPeople()
-    }
+  getPeople = async() => {
+      const doc = await Tabletop.init({
+          key: "https://docs.google.com/spreadsheets/d/1pz8JCy_ZvHgik3fNElwTSFybFvX-Iw1iR2-2-Nz-uXE/edit?usp=sharing",
+          simpleSheet: false
+      })
 
-    render() {
-        return (
-            <div>
-                <Header position="sticky" bg="white" />
-                <Container className="h-100 d-flex flex-column justify-content-center align-items-center pt-5">
-                    <Row>
-                        <h1 className="pri-font mb-5">Our Team</h1>
-                    </Row>
-                    <Row>
-                        {this.state.profiles.map((profile) => (
-                            <Col xs={12} md={6} lg={4} key={profile._rowNumber}>
-                                <ProfileCard
-                                    profile={profile}
-                                    url={this.props.match.url}
-                                />
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
-                <Footer />
-            </div>
-        );
-    }
+      this.setState({
+          profiles : doc.Agents.elements
+      })
+
+      console.log(doc.Agents.elements)
+  }
+
+  componentDidMount() {
+    this.getPeople();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header position="sticky" bg="white" />
+        <Container className="h-100 d-flex flex-column justify-content-center align-items-center pt-5">
+          <Row>
+            <h1 className="pri-font mb-5">Our Team</h1>
+          </Row>
+          <Row>
+            {this.state.profiles.map((profile) => (
+              <Col xs={12} md={6} lg={4} key={profile.row}>
+                <ProfileCard profile={profile} url={this.props.match.url} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,52 +1,43 @@
 import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
-import { GoogleSpreadsheet } from "google-spreadsheet";
+import Tabletop from "tabletop";
 
 import Header from "./micro-components/Header";
 
 class Profile extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
             isLoaded: false,
             person: {},
-            testimonials: []
-        }
+            testimonials: [],
+        };
     }
 
+
     getPeople = async () => {
-        const doc = new GoogleSpreadsheet(
-            process.env.REACT_APP_GOOGLE_SPREADSHEET_ID
-        );
-        await doc.useServiceAccountAuth({
-            private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
-            client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        const doc = await Tabletop.init({
+            key:
+                "https://docs.google.com/spreadsheets/d/1pz8JCy_ZvHgik3fNElwTSFybFvX-Iw1iR2-2-Nz-uXE/edit?usp=sharing",
+            simpleSheet: false,
         });
-
-        await doc.loadInfo();
-
-        const sheet = doc.sheetsByIndex[0];
-        const rows = await sheet.getRows();
-
-        const testimonials_sheet = doc.sheetsByIndex[1];
-        const testimonials_rows = await testimonials_sheet.getRows();
 
         this.setState({
             isLoaded: true,
-            person: rows.find(p => p.link === this.props.match.params.name),
-            testimonials: testimonials_rows.filter(t => t.link === this.props.match.params.name)
-        })
+            person: doc.Agents.elements[0],
+            testimonials: doc.Testimonials.elements,
+        });
+
+        console.log(doc.Agents.elements);
     };
 
     componentDidMount() {
-        this.getPeople()
+        this.getPeople();
     }
 
     render() {
         if (!this.state.isLoaded) {
-            return (
-                <h1>Loading</h1>
-            )
+            return <h1>Loading</h1>;
         } else {
             return (
                 <div>
@@ -54,28 +45,44 @@ class Profile extends React.Component {
                     <Container>
                         <Row>
                             <Col md={6}>
-                                <Image src={require(`../static/img/${this.state.person.img}`)} fluid />
+                                <Image
+                                    src={require(`../static/img/${this.state.person.img}`)}
+                                    fluid
+                                />
                             </Col>
-                            <Col md={6} className="d-flex flex-column justify-content-center align-items-center">
+                            <Col
+                                md={6}
+                                className="d-flex flex-column justify-content-center align-items-center"
+                            >
                                 <h3>{this.state.person.name}</h3>
-                                <p><em>{this.state.person.title}</em></p>
+                                <p>
+                                    <em>{this.state.person.title}</em>
+                                </p>
                                 <h5>{this.state.person.profile}</h5>
                             </Col>
                         </Row>
-                        {this.state.testimonials.map(testimonial => (
+                        {this.state.testimonials.map((testimonial) => (
                             <Row key={testimonial._rowNumber}>
-                                <Col md={6} className="d-flex flex-column justify-content-center align-items-center">
-                                   <h5>{testimonial.testimonial}</h5>
-                                   <p><em>{testimonial.client}</em></p>
+                                <Col
+                                    md={6}
+                                    className="d-flex flex-column justify-content-center align-items-center"
+                                >
+                                    <h5>{testimonial.testimonial}</h5>
+                                    <p>
+                                        <em>{testimonial.client}</em>
+                                    </p>
                                 </Col>
                                 <Col md={6}>
-                                    <Image src={require(`../static/img/${testimonial.img}`)} fluid />
+                                    <Image
+                                        src={require(`../static/img/${testimonial.img}`)}
+                                        fluid
+                                    />
                                 </Col>
                             </Row>
                         ))}
                     </Container>
                 </div>
-            )
+            );
         }
     }
 }
